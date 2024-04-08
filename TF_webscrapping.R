768e87e2f20fd133bc32dc35554ae0cc3ab92fb1
Andres-AM <andes.acunamarroquin@gmail.com>
2024-04-08 19:17:43 +0200

    udpate

--- TF_webscrapping.R
+++ TF_webscrapping.R
@@ -3,11 +3,29 @@
 source("../libraries.R")
 source("fun.R")
 
+# date_start = "2023-01-16"
+# date_end = "2023-01-16"
+# publication_filter = T
+# delay = 5 
+# mc.cores = 8
+# 
+# date_start = "2024-04-28"
+# date_end = "2024-04-28"  
 
 ## Function to use 
-decision_table <- get_TF_decision(date_start = "2024-03-19", date_end = "2024-03-19", delay = 5 , mc.cores = 8)$decision_table
+decision_table <- get_TF_decision(
+  date_start = "2024-03-01", 
+  date_end = "2024-03-16"  ,
+  publication_filter = T,
+  delay = 5 , 
+  mc.cores = 8
+);decision_table
+# 
+# $decision_table
 
 ## Writing the output of the functions in a csv table
 write_csv2(decision_table,"data/decision_table.csv")
 
 rm(list = ls()); gc()
+view(decision_table$recap_table)
+decision_table$decision_table[1,]$decision
--- fun.R
+++ fun.R
@@ -1,100 +1,14 @@
 
-### Extracting urls 
-get_url_day <- function(date_start = date_start , date_end = today()) {
-  
-  date_start = date(date_start)
-  date_end   = date(date_end)
-  
-  date <- seq(date_start,date_end,by ="1 day") 
-  
-  date_bis <- date %>% str_remove_all("-")
-  
-  url <- paste("https://www.bger.ch/ext/eurospider/live/fr/php/aza/http/index_aza.php?date=",date_bis,"&lang=fr&mode=news",sep = "")
-  # home_page <- "https://www.bger.ch/ext/eurospider/live/fr/php/aza/http/index_aza.php?lang=fr&mode=index"
-  
-  table <- tibble(date = date,url = url )
-  
-  return(table)
-  
-}
-
-
-get_url_decision <- function(i = i, per_day_url = per_day_url, delay = delay) {
-  
-  date <- per_day_url$date[[i]]
-  
-  url <- per_day_url$url[[i]]
-  
-  session <- polite::bow(url,delay = delay,force = T) 
-  
-  document <- polite::scrape(session)
-
-  if(length(document) != 2){df_temp <- tibble(date = date) ;return(df_temp)}
-
-    document_temp <- 
-      document %>% 
-      html_elements("#maincontent") 
-    
-    reference <- 
-      document_temp %>%
-      html_elements("a") %>% 
-      html_text() 
-    
-    url <-
-      document_temp %>%
-      html_elements("a") %>% 
-      html_attr("href") 
-    
-    type <-
-      document_temp %>% 
-      html_elements("cite") %>% 
-      html_text2()
-    
-    df_temp <- 
-      tibble(date = date, reference= reference,url= url)   %>%
-      filter(!reference %in% c("","Back","jour précédent","jour suivant","retour à la liste")) %>%
-      mutate( publication = str_detect(type,"\\*")) %>%
-      bind_cols(type = type) %>% 
-      select(publication,date,reference,type,url)
-    
-    # Sys.sleep(delay)
-    
-    return(df_temp)
-
-}
-
-get_text_decision <- function(i = i,url_decision = url_decision,user_agent = user_agent,delay = delay){
-  
-  decision_table_temp <- url_decision[i,]
-  
-  if(is.na(decision_table_temp$url)){decision_table_temp <- tibble(date = decision_table_temp$date) ;return(decision_table_temp)}
-  
-  session <- polite::bow(decision_table_temp$url,delay = delay,force = T) 
-  decision_temp <- polite::scrape(session)
-  
-  # decision_temp <- rvest::read_html(decision_table_temp$url)
-  
-  decision <-
-    decision_temp %>% 
-    html_elements(".content") %>% 
-    html_text()
-  
-  decision_table_temp <- decision_table_temp %>% 
-    mutate(decision = decision[1])
-  
-  # Sys.sleep(delay)
-  
-  return(decision_table_temp)
-  
-}
-
 ## Wrapper
-get_TF_decision <- function(date_start = date_start, date_end = date_end, delay = delay, mc.cores = mc.cores) {
+get_TF_decision <- function(date_start = date_start, date_end = date_end,
+                            publication_filter = publication_filter, 
+                            delay = delay, 
+                            mc.cores = mc.cores){
   
   ## Step 1, obtaining the URL for the list of decisions for the chosen days
   per_day_url <- get_url_day(date_start = date_start,date_end = date_end)
   
-  url_decision <-  
+  url_decision_raw <-  
     pbmclapply(         
       1:nrow(per_day_url),         ### .x
       get_url_decision,            ### FUN
@@ -102,9 +16,29 @@ get_TF_decision <- function(date_start = date_start, date_end = date_end, delay 
       mc.cores = mc.cores,
       delay = delay
     ) %>% 
-    map_dfr(~ .x)
+    map_dfr(~ .x) 
+  # browser()
   
+  recap_table <- url_decision_raw %>% 
+    group_by(date, publication,log) %>% 
+    count()
+  
+  url_decision <- url_decision_raw %>% 
+    filter(publication %in% c(publication_filter))
+  
+  
+  ### Necessary???
+  if(nrow(url_decision) == 0){ 
+    
+    url_decision <- per_day_url %>% bind_cols( log = "no valid page")
+    results <- list(url_decision = url_decision, decision_table = url_decision)
+    return(results)
+    
+  }
+  
+  # browser()
   ## Step 2, retrieving the decisions from the decision list and consolidating them into a table
+  # browser()
   decision_table <-  
     pbmclapply(         
       1:nrow(url_decision),           ### .x
@@ -115,8 +49,87 @@ get_TF_decision <- function(date_start = date_start, date_end = date_end, delay 
     ) %>% 
     map_dfr(~ .x)
   
-  results <- list(url_decision = url_decision, decision_table = decision_table)
+  results <- list(recap_table = recap_table, decision_table = decision_table, per_day_url = per_day_url)
   
   return(results)
+  
+}
+
+### Extracting urls 
+get_url_day <- function(date_start = date_start , date_end = today()) {
+  
+  date <- seq(date(date_start), date(date_end), by ="1 day") 
+  
+  date_vector <- date %>% str_remove_all("-")
+  
+  url <- paste("https://www.bger.ch/ext/eurospider/live/fr/php/aza/http/index_aza.php?date=",date_vector,"&lang=fr&mode=news",sep = "")
+  
+  table <- tibble(date = date, url = url )
+  
+  return(table)
+  
+}
+
+get_url_decision <- function(i = i, per_day_url = per_day_url, delay = delay){
+
+  df <- per_day_url[i,]
+  
+  session <- polite::bow(df$url,delay = delay,force = T) 
+  
+  document <- polite::scrape(session)
+  
+  if(length(document) != 2){df <- df %>% mutate(publication = F, log = "invalid page"); return(df)}
+  
+  document_temp <- 
+    document %>% 
+    html_elements("#maincontent") 
+  
+  reference <- 
+    document_temp %>%
+    html_elements("a") %>% 
+    html_text() 
+  
+  url <-
+    document_temp %>%
+    html_elements("a") %>% 
+    html_attr("href") 
+  
+  type <-
+    document_temp %>% 
+    html_elements("cite") %>% 
+    html_text2()
+  
+  df_temp <-
+    tibble(reference= reference, url= df$url, date = df$date,log = "valid page")   %>%
+    filter(!reference %in% c("","Back","jour précédent","jour suivant","retour à la liste")) %>%
+    mutate( type = type,publication = str_detect(type,"\\*"), ) %>%
+    select(publication,date,reference,type,log,url)
+  
+  if(nrow(df_temp) == 0){df <- df %>% mutate(publication = F, log = "valid page, no results"); return(df)}
+  
+  df <- df_temp
+  
+  return(df)
+  
+}
+
+get_text_decision <- function(i = i,url_decision = url_decision,user_agent = user_agent,delay = delay){
+  
+  decision_table_temp <- url_decision[i,]
+  
+  if(is.na(decision_table_temp$url)){decision_table_temp <- tibble(date = decision_table_temp$date, log ="valid page, no decision") ;return(decision_table_temp)}
+  
+  session <- polite::bow(decision_table_temp$url,delay = delay,force = T) 
+  decision_temp <- polite::scrape(session)
+  
+  decision <-
+    decision_temp %>% 
+    html_elements(".content") %>% 
+    html_text()
+  
+  decision_table_temp <- decision_table_temp %>% 
+    mutate(decision = decision[1])
+  
+  return(decision_table_temp)
   
 }
