076a424f706fcfd6e61b947b9ed4a311159a6877
Andres-AM <andes.acunamarroquin@gmail.com>
2024-04-11 19:02:30 +0200

    update

--- app.R
+++ app.R
@@ -1,14 +1,7 @@
-## app.R ##
-library(shinydashboard)
-
-library(shiny)
-library(bslib)
 
 ## app.R ##
-source("../libraries.R")
+source("libraries.R")
 source("fun.R")
-library(shinydashboard)
-library(promises)
 
 ui <- dashboardPage(skin = "red",
   dashboardHeader(title = "TFScrapper"),
@@ -17,8 +10,6 @@ ui <- dashboardPage(skin = "red",
     dateRangeInput(inputId = "date_range",label = "Date range:",start = "2024-03-01",end = "2024-03-01"),
     
     sliderInput(inputId = "delay",label = "Web Scrapping delay:",min = 5,max = 10,value = 5),
-    
-    # sliderInput(inputId = "mc_cores",label = "mc.cores to use:",min = 1,max = 8,value = 6),
     
     checkboxInput(inputId = "publication_filter",label = "Only select publications",value = T),
     
@@ -35,25 +26,24 @@ ui <- dashboardPage(skin = "red",
         h2("TF Scrapper"),
         "This web scraping tool is designed to retrieve the most recent case laws from the Swiss Federal Tribunal. It operates by parsing the content available at the specified website URL. It then organizes the extracted decisions, along with their corresponding texts, into a convenient table format."),
       
+      
+      box(
+        h2("Select Menu"),
+        selectInput(inputId = "subcategories",
+                    label = "reference",
+                    choices = NULL)
+        
+      ),
 
       box(
         h2("Output table"),
         DT::dataTableOutput(outputId = "df_output")),
-      
-      box(
-        h2("Select Menu"),
-        selectInput(inputId = "reference",
-                    label = "reference",
-                    choices = NULL)
 
-      )
-      
-      
     )
   )
 )
 
-server <- function(input, output) {
+server <- function(input, output,session) {
   
   output$args <- renderText({
     
@@ -72,16 +62,15 @@ server <- function(input, output) {
       date_end =  date(input$date_range[2]),
       publication_filter = input$publication_filter,
       delay = input$delay 
-      # mc.cores = input$mc_cores
       )
+    
+    updateSelectInput(session, "subcategories", choices = results$decision$reference)
     
     return(results )
   
   })
   
-  # output$table <- renderTable(output_table())
-  
-  output$df_output <- DT::renderDataTable(table_reac()$decision)
+  output$df_output <- DT::renderDataTable(table_reac()$recap_table)
   
 }
 
